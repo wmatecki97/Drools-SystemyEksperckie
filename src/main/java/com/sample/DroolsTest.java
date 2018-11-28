98c21178d7e48302dc2ba9d00bd3d5f5f79ad147
wmatecki97 <wmatecki97@gmail.com>
2018-11-28 09:24:43 +0100

    Basic Classes Added

--- src/main/java/com/sample/DroolsTest.java
+++ src/main/java/com/sample/DroolsTest.java
@@ -1,5 +1,7 @@
 package com.sample;
 
+import java.util.ArrayList;
+
 import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
@@ -17,41 +19,34 @@ public class DroolsTest {
         	KieSession kSession = kContainer.newKieSession("ksession-rules");
 
             // go !
-            Message message = new Message();
-            message.setMessage("Hello World");
-            message.setStatus(Message.HELLO);
-            kSession.insert(message);
+//            Message message = new Message();
+//            message.setMessage("Hello World");
+//            message.setStatus(Message.HELLO);
+//            kSession.insert(message);
             kSession.fireAllRules();
         } catch (Throwable t) {
             t.printStackTrace();
         }
     }
 
-    public static class Message {
-
-        public static final int HELLO = 0;
-        public static final int GOODBYE = 1;
-
-        private String message;
-
-        private int status;
-
-        public String getMessage() {
-            return this.message;
+    public static class Question {
+        public String content;
+        public ArrayList<String> answers;
+        public boolean proceeded;
+        
+        public Question() {
+        	answers = new ArrayList<String>();
         }
-
-        public void setMessage(String message) {
-            this.message = message;
-        }
-
-        public int getStatus() {
-            return this.status;
-        }
-
-        public void setStatus(int status) {
-            this.status = status;
-        }
-
     }
+
+    public static class Result{
+        ArrayList<String> results;
+        
+        public Result() {
+        	results = new ArrayList<String>();
+        }
+    }
+    
+
 
 }
