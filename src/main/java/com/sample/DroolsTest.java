dc97213da190babf2548a7ee6f906fa29f73b8d8
wmatecki97 <wmatecki97@gmail.com>
2018-12-10 22:45:05 +0100

    NewQuestionWindow Added

--- src/main/java/com/sample/DroolsTest.java
+++ src/main/java/com/sample/DroolsTest.java
@@ -1,44 +1,27 @@
 package com.sample;
-
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JRadioButton;
-
 import java.util.ArrayList;
 
 import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
-import java.awt.Container;
-
-import javax.swing.ButtonGroup;
-import javax.swing.JButton;
-import java.awt.event.*;
 /**
  * This is a sample class to launch a rule.
  */
 public class DroolsTest {
-	
+
 	
     
     public static final void main(String[] args) {
         try {
+//        	QuestionWindow.updateFrame("pytanie", "odpowiedz1", "odpowiedz2", "odpowiedz3");
 
-        	Frame.createFrame();
-        	Frame.updateFrame("pytanie1", "odpowiedz1", "odpowiedz2");
-        	Frame.updateFrame("pytanie2", "odpowiedz1", "odpowiedz2", "odpowiedz3");
-        	
+
         	/////////////////////////////////////////////
             // load up the knowledge base
 	        KieServices ks = KieServices.Factory.get();
     	    KieContainer kContainer = ks.getKieClasspathContainer();
         	KieSession kSession = kContainer.newKieSession("ksession-rules");
-//
-//            // go !
-////            Message message = new Message();
-////            message.setMessage("Hello World");
-////            message.setStatus(Message.HELLO);
-////            kSession.insert(message);
+
             kSession.fireAllRules();
         } catch (Throwable t) {
             t.printStackTrace();
---
+++ src/main/java/com/sample/QuestionWindow.java
@@ -0,0 +1,18 @@
+package com.sample;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class QuestionWindow {
+	public static JFrame frame;
+	
+	public static String updateFrame(String questionValue, List<String> options) {
+		int index = JOptionPane.showOptionDialog(null, questionValue,
+                "Wybierz odpowiedz",
+                JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options.toArray(new String[options.size()]), options.get(0));
+		
+		return options.get(0);
+	}
+}
